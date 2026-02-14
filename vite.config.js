278190961c1efbe485ecd43368c79a75df6f0394
unknown <marcomasg4@gmail,com>
2026-02-13 20:17:02 -0500

    quinto commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // For easy deployment as a static site
+    base: '/Dana/', // GitHub Pages base path
 })
